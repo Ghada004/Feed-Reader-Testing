6c9cfa094392222ade560295efcc3c4271da674f
Kevin <mchaletkevin@gmail.com>
2015-12-31 10:46:39 -0500

    style: Changes to spacing and indentation

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -35,9 +34,9 @@ $(function() {
             });
         });
 
-         // This test looped through each feed and determines that each
-         // feed has a name and not empty.
-         it('name defined and not empty', function() {
+        // This test looped through each feed and determines that each
+        // feed has a name and not empty.
+        it('name defined and not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
                 expect(typeof feed.name).toEqual('string');
@@ -50,13 +49,13 @@ $(function() {
     // Menu test suite.
     describe('The menu', function() {
 
-         // This test ensures the menu element is hidden by default.
-         it('is hidden by default', function() {
+        // This test ensures the menu element is hidden by default.
+        it('is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
-         });
+        });
 
-          // This test validates proper functioning of the hamburger menu toggle.
-          it('changes visibility when icon clicked', function() {
+        // This test validates proper functioning of the hamburger menu toggle.
+        it('changes visibility when icon clicked', function() {
             var hamburger = $('.menu-icon-link');
 
             // This tests for menu display.
@@ -66,7 +65,7 @@ $(function() {
             // This tests for menu hide.
             hamburger.click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
+        });
     });
 
     // Initial entries test suite.
@@ -80,9 +79,8 @@ $(function() {
         });
 
         // tests that there is at least one entry in feed.
-        it('should be called and contain at least one feed.', function(done) {
+        it('should be called and contain at least one feed.', function() {
             expect($('.entry').length).toBeGreaterThan(0);
-            done();
         });
     });
 
