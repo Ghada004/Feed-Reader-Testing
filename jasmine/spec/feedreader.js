a04605a93ee0786bb7ecac2adc17b76bea20c443
Kevin <mchaletkevin@gmail.com>
2015-12-31 10:50:23 -0500

    docs: removed unneeded info from 'are defined' description.
    docs: removed unneeded info from 'are defined' description.
    
    This info was helpful when starting the project, but serves no purpose
    currently.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -15,10 +15,7 @@ $(function() {
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
